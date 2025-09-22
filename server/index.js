4edba81182964e4fc4b7b25549223179be21e60b
UsmanGhani21-svg <Usmanghani50985@gmailcom>
2025-09-22 17:31:45 +0500

    Fix: updated server/index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ socketHandler(io);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+server.listen(PORT,"0.0.0.0", () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
 
